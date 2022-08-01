ee8654af187dea0fdd60a6be5d9ae751cb0c7d26
Hossam <hossamibrahim66666@gmail.com>
2022-08-01 21:31:25 +0200

    add dashboard

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -252,14 +252,14 @@
 		</div>
 	</footer>
 	
-	<script src="{{asset('js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{asset('js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{asset('js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('js/jquery.flexslider.js')}}"></script>
-	<script src="{{asset('js/chosen.jquery.min.js')}}"></script>
-	<script src="{{asset('js/owl.carousel.min.js')}}"></script>
-	<script src="{{asset('js/jquery.countdown.min.js')}}"></script>
-	<script src="{{asset('js/jquery.sticky.js')}}"></script>
-	<script src="{{asset('js/functions.js')}}"></script>
+	<script src="{{asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+	<script src="{{asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('assets/js/jquery.flexslider.js')}}"></script>
+	<script src="{{asset('assets/js/chosen.jquery.min.js')}}"></script>
+	<script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+	<script src="{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+	<script src="{{asset('assets/js/jquery.sticky.js')}}"></script>
+	<script src="{{asset('assets/js/functions.js')}}"></script>
 </body>
 </html>
